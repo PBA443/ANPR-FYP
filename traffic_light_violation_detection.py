575500a80c72268c3c12b4b2a0007464b9920192
PBA443 <110591161+PBA443@users.noreply.github.com>
2023-09-16 10:46:09 +0530

    Update traffic_light_violation_detection.py

--- traffic_light_violation_detection.py
+++ traffic_light_violation_detection.py
@@ -7,9 +7,15 @@ import os
 import time
 from util import read_license_plate
 import pyrebase
-from firebase_admin import storage
-from firebase_admin import db
+
 from datetime import datetime
+
+'''def POINTS(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE:
+        colorsBGR = [x, y]
+        print(colorsBGR)
+cv2.namedWindow('YOLOv8 Tracking')
+cv2.setMouseCallback('YOLOv8 Tracking', POINTS)'''
 
 front_light_violation1 = 'images/front_traffic_light/number_plate'
 if not os.path.exists(front_light_violation1):
@@ -68,9 +74,9 @@ def newassign_ids_and_coordinates_to_tracked_objects(boxes, v_track_id, v_det_co
 
 
 def detect_green_color(frame):
-    traffic_signal = [(345, 116), (344, 183), (372, 184), (372, 113)]
+    traffic_signal = [(380,74), (380,161), (408,161), (408,74)]
     green = [0, 0, 255]
-    plate_crop = frame[116:183, 345:372, :]
+    plate_crop = frame[74:161, 381:408, :]
     hsvImg = cv2.cvtColor(plate_crop, cv2.COLOR_BGR2HSV)
     lower_limit, upper_limit = get_limits(color=green)
     mask = cv2.inRange(hsvImg, lower_limit, upper_limit)
@@ -82,37 +88,12 @@ def detect_green_color(frame):
     cv2.polylines(frame, [np.array(traffic_signal, np.int32)], True, (0, 0, 255), 2)
     return bbox is not None
 
-
-# firebase
-config = {
-    'apiKey': "AIzaSyD_NFy6pD8-gnQJUMqKbPF8qux7IYz8H_g",
-    'authDomain': "research-project-811a8.firebaseapp.com",
-    'databaseURL': "https://research-project-811a8-default-rtdb.asia-southeast1.firebasedatabase.app",
-    'projectId': "research-project-811a8",
-    'storageBucket': "research-project-811a8.appspot.com",
-    'messagingSenderId': "792324726693",
-    'appId': "1:792324726693:web:94ef9d2bc5d1558724924c",
-    'measurementId': "G-77N9TYF9N3",
-    'serviceAccount': "credentials.json",
-}
-
-firebase = pyrebase.initialize_app(config)
-storage = firebase.storage()
-db = firebase.database()
-
-# set location
-location = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15857.823186462296!2d80.08955465947969!3d6.4637958678341905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae22d305126dacb%3A0xb503df7f647ef4cd!2sWelipenna%20Interchange!5e0!3m2!1sen!2slk!4v1693501904033!5m2!1sen!2slk"
-
-# get time
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-
 # output variables
-output_video_path = "output_video.mp4"
-output_video_fps = 30  # Choose the desired frame rate
-fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Codec for the output video
+#output_video_path = "dewata_red_light_violation.MP4"
+#output_video_fps = 30  # Choose the desired frame rate
+#fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Codec for the output video
 # Initialize the video writer
-out = cv2.VideoWriter(output_video_path, fourcc, output_video_fps, (1020, 600))
+#out = cv2.VideoWriter(output_video_path, fourcc, output_video_fps, (1020, 600))
 
 # variables
 offset = 6
@@ -120,13 +101,13 @@ go_up = {}
 red_light = {}
 red_light_counter = []
 up_counter = []
-red_light_violation_area = [(380, 481), (129, 594), (912, 593), (754, 481)]
+red_light_violation_area = [(277,386), (214,478), (900,497), (775,396)]
 vehicle_types = [2]
 down = {}
 down_counter_direction = []
 model = YOLO('yolov8l.pt')
 p_model = YOLO('last.pt')
-video_path = "Front.mp4"
+video_path = "dewata_red_light_violation.MP4"
 cap = cv2.VideoCapture(video_path)
 light_violated_count = []
 
@@ -186,27 +167,18 @@ while cap.isOpened():
             if detected_green == True:
                 cv2.polylines(frame, [np.array(red_light_violation_area, np.int32)], True, (0, 0, 255), 1)
                 if red_light_violation_detection >= 0 and conf1 > 60:
-
                     cv2.circle(frame, (c_x, c_y), 2, (0, 0, 255), -1)
                     cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
                     cv2.rectangle(frame, (int(x3), int(y3)), (int(x4), int(y4)), (255, 0, 0), 2)
                     # cv2.putText(frame, str(v_id), (x1, y1 - 2), 0, 1, (255, 255, 255), thickness=1, lineType=cv2.LINE_AA)
                     frame_image_filename = os.path.join(front_light_violation2, f'front_traffic_light_{v_id}.jpg')
                     image_trafficlight_frame = os.path.join(front_light_violation2, f'front_traffic_light_{v_id}.jpg')
-                    data_frontredlight = {"violation": "redlightfront_violation", "ID": v_id, "Time": current_time,
-                                          "Location": location}
                     cv2.imwrite(frame_image_filename, frame)
-                    storage.child(image_trafficlight_frame).put(image_trafficlight_frame)
-                    db.child("Violation_Details_front").child(v_id).set(data_frontredlight)
-                    red_light[v_id] = c_y
-                    noi_frontredlight = {"NOI": len(red_light)}
-                    db.child("NumberOf_frontredlight_violation").set(noi_frontredlight)
                     # vehicle
                     RLV_vehicle_crop = frame[int(y1):int(y2), int(x1):int(x2), :]
                     RLV_vehicle_image_filename = os.path.join(front_light_violation3, f'front_traffic_light_{v_id}.jpg')
                     image_trafficlight_vehicle = os.path.join(front_light_violation3, f'front_traffic_light_{v_id}.jpg')
                     cv2.imwrite(RLV_vehicle_image_filename, RLV_vehicle_crop)
-                    storage.child(image_trafficlight_vehicle).put(image_trafficlight_vehicle)
 
                     # plate
                     RLV_plate_crop = frame[int(y3):int(y4), int(x3):int(x4), :]
@@ -214,20 +186,19 @@ while cap.isOpened():
                     image_trafficlight_plate = os.path.join(front_light_violation1, f'front_traffic_light_{v_id}.jpg')
                     if RLV_plate_crop is not None and not RLV_plate_crop.size == 0:
                         cv2.imwrite(RLV_plate_image_filename, RLV_plate_crop)
-                        storage.child(image_trafficlight_plate).put(image_trafficlight_plate)
                         # cv2.imshow('red light violated vehicle plates', RLV_plate_crop)
                         RLV_plate_crop_gray = cv2.cvtColor(RLV_plate_crop, cv2.COLOR_BGR2GRAY)
                         _, plate_crop_thresh = cv2.threshold(RLV_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
                         plate_text, plate_confident_val = read_license_plate(plate_crop_thresh)
                         print(plate_text)
-        cv2.imshow("YOLOv8 Tracking", frame)
-        out.write(frame)
+        cv2.imshow("Red Light Violation", frame)
+        #out.write(frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
     else:
         # Break the loop if the end of the video is reached
         break
 # Release the video capture object and close the display window
-out.release()
+#out.release()
 cap.release()
 cv2.destroyAllWindows()
